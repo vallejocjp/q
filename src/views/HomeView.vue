b155925e70814ffa48b41dbd5c41e9d1d2c8b4ae
Juan Pablo Vallejo <vallejocjp28@gmail.com>
2023-04-28 10:12:50 -0500

    Initial styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
+<script>
+</script>
+
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>La identificación institucional presentada es válida</h1>
+    <img src="https://www.columbus.edu.co/wp-content/uploads/Columbus-logo.png" alt="logo">
   </div>
+  <footer>
+    <h3>The Columbus School {{ new Date().getFullYear() }}</h3>
+  </footer>
 </template>
 
-<script>
 
-</script>
+
+<style scoped>
+
+h2 {    
+  color: #002169;
+  margin: 0;
+    }
+
+h3 {
+  color: white;
+}
+
+footer {
+  position: fixed;
+    height: 50px;
+    background-color: #002169;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    margin-bottom: 0px;
+}
+</style>
